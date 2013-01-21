5584238d175765e4b33f6afaf4375fe80d0269e8
Chris Clarke <chrisclarke1977@gmail.com>
2013-01-21 10:58:55 -0500

    Changes to folder name and readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ doctors-pa
 ==========
 
 Open development of in patient task management application
+
+This is a MongoDB and Node.js application using Socket.io and Express
--- smart-ward 2/models/bed.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-var BedSchema = new mongoose.Schema({
-	name: {type: String, required: true}
-});
-
-module.exports = mongoose.model('Bed', BedSchema);
